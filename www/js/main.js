84600ecd335cfa4bf5360f2d317e2c03bd16aa3e
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-08-17 19:57:52 -0400

    trying to get some initial code to work

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,12 @@
 var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+      /*  this.store = new MemoryStore();
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));*/
+    },
 
-    findByName: function() {
+   /* findByName: function() {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(orgs) {
             var l = orgs.length;
@@ -11,13 +17,31 @@ var app = {
                 $('.org-list').append('<li><a href="#orgs/' + e.id + '">' + e.Name + '</a></li>');
             }
         });
+    },*/
+
+    // Bind Event Listeners
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        /*document.addEventListener('online', this. onOnline, true);*/
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+       // var parentElement = document.getElementById(id);
+       // var listeningElement = parentElement.querySelector('.listening');
+       // var receivedElement = parentElement.querySelector('.received');
+
+       // listeningElement.setAttribute('style', 'display:none;');
+//receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ');
     }
+    
 
 };
 
-app.initialize();
