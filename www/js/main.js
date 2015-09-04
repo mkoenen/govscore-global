45b9d9e4f3001a6c010b6f742c027cb4d27863bb
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-09-04 16:25:57 -0400

    uncommented pudhwoodh code

--- www/js/main.js
+++ www/js/main.js
@@ -605,78 +605,78 @@ function calcResults() {
 
 
 /* Pushwoosh ---------------------------------------------------*/
-// function initPushwoosh() {
-//       var pushNotification = window.plugins.pushNotification;
+function initPushwoosh() {
+      var pushNotification = window.plugins.pushNotification;
 
-//       if(device.platform == "Android")
-//       {
-//         registerPushwooshAndroid();
-//       }else if(device.platform == "iPhone" || device.platform == "iOS")
-//       {
-//         registerPushwooshIOS();
-//       }
-//     }
+      if(device.platform == "Android")
+      {
+        registerPushwooshAndroid();
+      }else if(device.platform == "iPhone" || device.platform == "iOS")
+      {
+        registerPushwooshIOS();
+      }
+    }
 
-// function registerPushwooshAndroid() {
-//     var pushNotification = window.plugins.pushNotification;
+function registerPushwooshAndroid() {
+    var pushNotification = window.plugins.pushNotification;
  
-//     //set push notifications handler
-//     document.addEventListener('push-notification', function(event) {
-//         var title = event.notification.title;
-//         var userData = event.notification.userdata;
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
                                  
-//         if(typeof(userData) != "undefined") {
-//             console.warn('user data: ' + JSON.stringify(userData));
-//         }
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
                                      
-//         alert(title);
-//     });
+        alert(title);
+    });
  
-//     //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
-//     pushNotification.onDeviceReady({ projectid: "864197909703", pw_appid : "4C804-675D6" });
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "864197909703", pw_appid : "4C804-675D6" });
  
-//     //register for pushes
-//     pushNotification.registerDevice(
-//         function(status) {
-//             var pushToken = status;
-//             console.warn('push token: ' + pushToken);
-//         },
-//         function(status) {
-//             console.warn(JSON.stringify(['failed to register ', status]));
-//         }
-//     );
-// }
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+}
 
-// function registerPushwooshIOS() {
-//     var pushNotification = window.plugins.pushNotification;
+function registerPushwooshIOS() {
+    var pushNotification = window.plugins.pushNotification;
  
-//     //set push notification callback before we initialize the plugin
-//     document.addEventListener('push-notification', function(event) {
-//                                 //get the notification payload
-//                                 var notification = event.notification;
+    //set push notification callback before we initialize the plugin
+    document.addEventListener('push-notification', function(event) {
+                                //get the notification payload
+                                var notification = event.notification;
  
-//                                 //display alert to the user for example
-//                                 alert(notification.aps.alert);
+                                //display alert to the user for example
+                                alert(notification.aps.alert);
                                
-//                                 //clear the app badge
-//                                 pushNotification.setApplicationIconBadgeNumber(0);
-//                             });
+                                //clear the app badge
+                                pushNotification.setApplicationIconBadgeNumber(0);
+                            });
  
-//     //initialize the plugin
-//     pushNotification.onDeviceReady({pw_appid:"4C804-675D6"});
+    //initialize the plugin
+    pushNotification.onDeviceReady({pw_appid:"4C804-675D6"});
      
-//     //register for pushes
-//     pushNotification.registerDevice(
-//         function(status) {
-//             var deviceToken = status['deviceToken'];
-//             console.warn('registerDevice: ' + deviceToken);
-//         },
-//         function(status) {
-//             console.warn('failed to register : ' + JSON.stringify(status));
-//             //alert(JSON.stringify(['failed to register ', status]));
-//         }
-//     );
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var deviceToken = status['deviceToken'];
+            console.warn('registerDevice: ' + deviceToken);
+        },
+        function(status) {
+            console.warn('failed to register : ' + JSON.stringify(status));
+            //alert(JSON.stringify(['failed to register ', status]));
+        }
+    );
      
-//     //reset badges on app start
-//     pushNotification.setApplicationIconBadgeNumber(0);
-// }
+    //reset badges on app start
+    pushNotification.setApplicationIconBadgeNumber(0);
+}
