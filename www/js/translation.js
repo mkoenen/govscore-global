59ae0dd9510b0a57d0bc6e3353605ed143b7efdd
Margarete Koenen <margarete.koenen@loudfeed.com>
2015-09-04 18:28:11 -0400

    trying to get translation working

--- www/js/translation.js
+++ www/js/translation.js
@@ -1,6 +1,17 @@
 i18n_dict = {
-    "Example 1"               : "Hallo!",
     
+    "about" : "About",
+    "about-text" : "GovScore is a governance maturity assessment tool designed to assist health leaders and managers improve their organizational performance through periodic assessment and continuous enhancement of their governance.",
+    "steps" : "Steps",
+    "step1" : "Answer the 25 questions in the GovScore survey and click Submit",
+    "step2" : "View your results on your phone.",
+    "step3" : "Invite other board members to download App and complete GovScore survey.",
+    "step4" : "Examine the combined result of all evaluators of your organization at <a href='http://govscoreapp.net'>govscoreapp.net</a> (a unique institutional code will have been provided in the App’s results tab). View individual analysis for your institution and recommendations for next improvement steps.",
+    "step5" : "Present GovScore analysis and recommendations to your board colleagues.",
+    "step6" : "Answer the Advanced GovScore questions on your app for improvement areas identified for your organization.",
+    "step7" : "View your results on your phone.",
+    "step8" : "Examine the combined Advanced GovScore result of all evaluators of your organization on govscoreapp.net and get further individualized analysis and recommendations for improvement. See how your organizations results compares to the aggregate results of all organizations.",
+    "step-last" : "Good governance leads to effective and efficient management of people, money, medicines and information. Sound management in turn facilitates the work of doctors, nurses, other clinicians and health workers and enables them to provide safe, timely, effective, and efficient care and services that are respectful of and responsive to individual patient and health service user preferences, needs, and values. Good governance essentially assures sound management which in turn enables reliable service delivery and higher organizational performance.",
   };
 
   i18n_dict_fr = {
